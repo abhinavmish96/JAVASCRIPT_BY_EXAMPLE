64b42f47ad9fa595ee1aef64e8fc65c9e66e6915
Abhinav Mishra <abhinavmish96@gmail.com>
2019-11-14 11:02:55 +0530

    <whileloop>

---
+++ whileloop.js
@@ -0,0 +1,28 @@
+// Like a basic for loop
+var i = 0;
+while (i < 10) {
+  console.log('i === ' + i)
+  i++;
+}
+
+// do-while loops evaluate the condition after the block is executed
+var j = 0;
+do {
+  console.log('j === ' + j)
+  j++;
+} while (j > 5)
+
+/*
+$ node jsbyexample.js
+i === 0
+i === 1
+i === 2
+i === 3
+i === 4
+i === 5
+i === 6
+i === 7
+i === 8
+i === 9
+j === 0
+*/
