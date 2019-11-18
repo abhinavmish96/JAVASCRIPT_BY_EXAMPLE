a992f9433b66652fb7f003e5d67d5d5b88e32ec4
Abhinav Mishra <abhinavmish96@gmail.com>
2019-11-18 10:51:45 +0530

    <switch>

---
+++ switch.js
@@ -0,0 +1,24 @@
+// a simple switch statment
+const fruit = 'banana'
+switch (fruit) {
+  case 'blueberry':
+    console.log('blue')
+    // everything after the true case executes
+    // use 'break' if you aren't returning a value
+    break;
+  case 'banana':
+    console.log('yellow')
+    break;
+    // multiple cases can resolve to the same branch
+  case 'apple':
+  case 'raspberry':
+    console.log('red')
+    break;
+  default:
+    console.log('not blue, yellow or red')
+}
+
+/*
+$ node jsbyexample.js
+yellow
+*/
