17675bd15bd042bf2a2de86c76aa23c800f50fb1
Abhinav Mishra <abhinavmish96@gmail.com>
2019-12-12 15:30:44 +0530

    <high-order-func>

---
+++ high-order-func.js
@@ -0,0 +1,27 @@
+// functions can take other functions as arguments
+// the built in setTimeout is the classic example
+function executeACallback(callback, arg) {
+    // this will envoke the function
+    // with whatever arg you pass it
+    return callback(arg)
+   }
+   
+   const add2 = (num) => num + 2
+   
+   console.log(executeACallback(add2, 3))
+   // => 5
+   
+   // functions can also return other functions
+   function isGreaterThan (num1) {
+    return num2 => num2 > num1
+   }
+   
+   let isGreaterThan10 = isGreaterThan(10)
+   console.log(isGreaterThan10(11))
+   // => true
+
+   /*
+   $ node jsbyexample.js
+   5
+   true
+   */
